7052a0636a4f4db0ee721f2d453bd8eeb1aa3265
Gustavo Escobar Preto <gescobarpreto@gmail.com>
2019-08-27 17:04:04 -0300

    aaaa

--- Lotofacil.bash
+++ Lotofacil.bash
@@ -9,10 +9,12 @@ unzip D_lotfac.zip
 rm D_lotfac.zip
 
 
-w3m -dump d_lotfac.htm > dados.txt
+#w3m -dump d_lotfac.htm > dados.txt
 
-grep "^[0-9]" dados.txt > dados1.txt
+#grep "^[0-9]" dados.txt > dados1.txt
 
-cat dados1.txt | cut -d, -f1-17 > dados2.txt
+#w3m -dump d_lotfac.htm |grep "^[0-9]" |tr -s " "|sed -e 's/^[ t]*//' -e 's/ /,/g' > dados.txt
 
+#cat dados.txt | cut -d, -f1-17 > dados1.txt
 
+w3m -dump d_lotfac.htm |grep "^[0-9]" |tr -s " "|sed -e 's/^[ t]*//' -e 's/ /,/g' |cut -d, -f1-17
